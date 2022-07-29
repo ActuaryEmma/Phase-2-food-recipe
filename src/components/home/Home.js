fbafd2b8f834ea04ce386eb6d652398ce80f2b62
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-07-29 17:29:54 +0300

    Homepage

---
+++ src/components/home/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import page from "../images/page.jpg";
+
+
+function Home() {
+  return (
+    <div>
+      <div className="p-3 mb-2 bg-light ">
+      <figure className="container position-relative  d-flex justify-content-around align-items-center ">
+        <img
+          src={page}
+          id="homepage"
+          className="img-fluid "
+          style={{ margin: `${50}px` }}
+          alt="homepage"
+        />
+        <div>
+          <figcaption className="hometitle">
+            The best part of dinner is the dessert, and it usually arrives when
+            you can't eat anymore
+            <button className="btn btn-info  ">Get Recipe</button>
+          </figcaption>
+        </div>
+      </figure>
+    </div>
+    
+    </div>
+    
+  );
+}
+
+export default Home;
